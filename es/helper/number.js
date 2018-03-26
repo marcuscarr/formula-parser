9dcce2c5aa73ca584c5f3dae7b80ea316fd7ba68
Marcus Carr <marcus.carr@gmail.com>
2018-03-26 17:46:04 -0600

    Add es and lib to git repo

---
+++ es/helper/number.js
@@ -0,0 +1,32 @@
+/**
+ * Convert value into number.
+ *
+ * @param {String|Number} number
+ * @returns {*}
+ */
+export function toNumber(number) {
+  var result = void 0;
+
+  if (typeof number === 'number') {
+    result = number;
+  } else if (typeof number === 'string') {
+    result = number.indexOf('.') > -1 ? parseFloat(number) : parseInt(number, 10);
+  } else if (number instanceof Date) {
+    // Excel serial dates are expressed as number of days since January 1, 1900
+    var unix = number.getTime() / 1000;
+    var excel = unix / 86400 + 25569;
+    result = excel;
+  }
+
+  return result;
+}
+
+/**
+ * Invert provided number.
+ *
+ * @param {Number} number
+ * @returns {Number} Returns inverted number.
+ */
+export function invertNumber(number) {
+  return -1 * toNumber(number);
+}
