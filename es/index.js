8e6c883fa72499a70dcffe4c3e649470cc291639
Marcus Carr <mcarr@riffyn.com>
2019-08-06 16:57:28 -0600

    Add dist es lib to gitignore

--- es/index.js
+++
@@ -1,5 +0,0 @@
-import Parser from './parser';
-import SUPPORTED_FORMULAS from './supported-formulas';
-import error, { ERROR, ERROR_DIV_ZERO, ERROR_NAME, ERROR_NOT_AVAILABLE, ERROR_NULL, ERROR_NUM, ERROR_REF, ERROR_VALUE } from './error';
-
-export { SUPPORTED_FORMULAS, ERROR, ERROR_DIV_ZERO, ERROR_NAME, ERROR_NOT_AVAILABLE, ERROR_NULL, ERROR_NUM, ERROR_REF, ERROR_VALUE, Parser, error };
